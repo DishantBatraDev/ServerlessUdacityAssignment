b937a87fa5c1d1c75f2e502f2536e3a953b1d6d6
DishantBatraDev <dishant.batra2015@gmail.com>
2020-09-28 11:43:14 +0530

    updated call back url

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,5 +6,5 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-l6h-4rus.us.auth0.com',            // Auth0 domain
   clientId: '0q8Yt6PEE2FAif8zibrn4cW0hD2tf8jQ',          // Auth0 client id
-  callbackUrl: 'http://localhost:8080/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 }
